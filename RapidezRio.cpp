c0877b2ff21f61e02dfcaffec980a81facb3191a
KevinAron31 <60454838+KevinAron31@users.noreply.github.com>
2020-02-03 21:41:18 -0800

    Create RapidezRio.cpp

---
+++ RapidezRio.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	// declaración de variables.
+	int y;
+	y=4;
+	int z;
+	z=12;
+	int total;
+	
+	//Resultado de la rapidez del rio.
+	total=((24-z)/(y));
+	std::cout<<"Las rapidez de la corriente es en m/h:"<<total<<::std::endl;
+
+}
