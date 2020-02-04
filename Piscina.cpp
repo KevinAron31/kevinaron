4018f0dd67175f5b21138d39e8c187c4ddd74977
KevinAron31 <60454838+KevinAron31@users.noreply.github.com>
2020-02-03 22:28:20 -0800

    Create Piscina.cpp

---
+++ Piscina.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	// declaración de variables.
+    int carrera;
+    carrera=1;
+    
+	int nado;
+	nado=5;
+	
+	int anchopis;
+	anchopis=40;
+	
+	int largopis;
+	largopis=13;
+	
+	int corrido;
+	int nadado;
+	int promedio;
+	
+	//Resultados.
+	corrido=(carrera*largopis);
+	std::cout<<"El total recorrido corriendo fue:"<<corrido<<::std::endl;
+    nadado=(anchopis/nado);
+    std::cout<<"El total recorrido nadado fue:"<<nadado<<::std::endl;
+    //falta resolver problema del promedio :c
+}
